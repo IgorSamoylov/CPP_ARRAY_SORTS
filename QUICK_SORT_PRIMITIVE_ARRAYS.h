74f4d67c89051a2ca43fe747b3bb94ec48b367af
IgorSamoylov <igorletterbox@gmail.com>
2021-10-07 09:23:27 +0500

    Add auxiliary insertion sort for extreme cases in QuickSort for Templates algorithm

--- QUICK_SORT_PRIMITIVE_ARRAYS.h
+++ QUICK_SORT_PRIMITIVE_ARRAYS.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "ARRAY_SORTS.h"
+#include <stack>
 
-// Cyclic implementation for QuickSort algorithm with M(1) memory usage
+// Iterative implementation for QuickSort algorithm with M(1) memory usage
 
-/*
+struct bound_ptrs {
+	size_t left, right;
+	bound_ptrs(size_t left, size_t right) : left(left), right(right) {}
+};
+
+
 template <class T>
 void quick_sort_prime(T* v, size_t size) {
 
@@ -13,14 +19,23 @@ void quick_sort_prime(T* v, size_t size) {
 	size_t pivot_index = 0;
 	T pivot_value = v[pivot_index];
 	size_t left_side = 0, right_side = size - 1, pivot_val_counter = 0;
+	stack<bound_ptrs> ptr_stack;
+	ptr_stack.push(bound_ptrs(left_side, right_side));
 	
-	
-	for (size_t i = 0; i < size; ++i) {
-		T val = v[i];
-		if (val < pivot_value) buffer[left_side++] = val;
-		else if (val == pivot_value) ++pivot_val_counter;
-		else buffer[right_side--] = val;
+	while (ptr_stack.size() > 0) {
+		bound_ptrs current_ptrs = ptr_stack.top(); ptr_stack.pop();
+		size_t left = current_ptrs.left;
+		size_t right = current_ptrs.right;
+		for (size_t i = 0; i < size; ++i) {
+			T val = v[i];
+			if (val < pivot_value) buffer[left++] = val;
+			else if (val == pivot_value) ++pivot_val_counter;
+			else buffer[right--] = val;
+		}
+		if(left -current_ptrs.left > 0)
+			ptr_stack.push(bound_ptrs(current_ptrs.left, left));
+		if (current_ptrs.right - right > 0)
+			ptr_stack.push(bound_ptrs(current_ptrs.right, right));
 	}
 }
 
-*/
--- QUICK_SORT_PRIMITIVE_ARRAYS_TEMPLATES_RECURSIVE.h
+++ QUICK_SORT_PRIMITIVE_ARRAYS_TEMPLATES_RECURSIVE.h
@@ -4,48 +4,92 @@
 
 // Quick sort recursive algorithm for Heap allocation primitive T array
 
+// Auxiliary sorts for extreme cases
+template <class T>
+void insertion_sort(T* v, size_t size) {
+
+	for (size_t i = 1; i < size; ++i) {
+		size_t t = i;
+		while (t > 0 && v[t - 1] > v[t]) {
+			T tmp = v[t];
+			v[t] = v[t - 1];
+			v[t - 1] = tmp;
+			//swap(v[t], v[t - 1]);
+			t--;
+		}
+	}
+}
+
+template <class T>
+void bubble_sort(T* v, size_t size) {
+	
+	bool flag = true;
+	while (flag) {
+		flag = false;
+		for (size_t i = 0; i < size - 1; ++i)
+			if (v[i] > v[i + 1]) {
+				//swap(v[i], v[i + 1]);
+				T tmp = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = tmp;
+				flag = true;
+			}
+	}
+}
+
 template <class T>
 void quick_sort_prim_t(T* v, size_t size) {
+	/*
+	if (size < 2) return;
+	else if (size == 2) {
+		if (v[0] > v[1]) {
+			int tmp = v[0];
+			v[0] = v[1];
+			v[1] = tmp;
+		}
+		return;
+	}*/
+	if (size < 40) {
+		insertion_sort(v, size);
+		return;
+	}
+	/*else if (size < 55) {
+		bubble_sort(v, size);
+		return;
+	}*/
 
-    if (size < 2) return;
-    else if (size == 2) {
-        if (v[0] > v[1]) {
-            int tmp = v[0];
-            v[0] = v[1];
-            v[1] = tmp;
-        }
-        return;
-    }
+	T pivot_value = *v;
+	size_t pivot_values_counter = 0;
 
-    T pivot_value = *v;
-    size_t pivot_values_counter = 0;
+	T* lower_values = new T[size]; size_t lower_size = 0;
+	T* higher_values = new T[size]; size_t higher_size = 0;
 
-    T* lower_values = new T[size]; size_t lower_size = 0;
-    T* higher_values = new T[size]; size_t higher_size = 0;
+	for (size_t i = 0; i < size; ++i) {
+		T val = *(v + i);
+		if (val < pivot_value) lower_values[lower_size++] = val;
+		else if (val == pivot_value) pivot_values_counter++;
+		else higher_values[higher_size++] = val;
+	}
 
-    for (size_t i = 0; i < size; ++i) {
-        T val = *(v + i);
-        if (val < pivot_value) lower_values[lower_size++] = val;
-        else if (val == pivot_value) pivot_values_counter++;
-        else higher_values[higher_size++] = val;
-    }
+	quick_sort_prim_t(lower_values, lower_size);
+	quick_sort_prim_t(higher_values, higher_size);
 
-    quick_sort_prim_t(lower_values, lower_size);
-    quick_sort_prim_t(higher_values, higher_size);
+	// Fill the pivot values after lower values into lower values array
+	for (size_t i = lower_size; i < lower_size + pivot_values_counter; ++i)
+		lower_values[i] = pivot_value;
 
-    // Fill the pivot values after lower values into lower values array
-    for (size_t i = lower_size; i < lower_size + pivot_values_counter; ++i)
-        lower_values[i] = pivot_value;
+	// Append higher values after lower and pivot values into lower values array
+	constexpr size_t item_size = sizeof(*v);
+	memcpy_s(lower_values + lower_size + pivot_values_counter, higher_size * item_size,
+		higher_values, higher_size * item_size);
 
-    // Append higher values after lower and pivot values into lower values array
-    constexpr size_t item_size = sizeof(*v);
-    memcpy_s(lower_values + lower_size + pivot_values_counter, higher_size * item_size,
-        higher_values, higher_size * item_size);
+	delete[](higher_values);
 
-    delete[](higher_values);
+	// Copy lower values array to the source array
+	memcpy_s(v, size * item_size, lower_values, size * item_size);
 
-    // Copy lower values array to the source array
-    memcpy_s(v, size * item_size, lower_values, size * item_size);
-    
-    delete[](lower_values);
+	delete[](lower_values);
 }
+
+
+
